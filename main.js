22295a1553232306e0bac8370a8d0606ab2f541d
Haruto Hirakawa <31243896+jpnykw@users.noreply.github.com>
2021-05-10 07:27:04 +0900

    :recycle: Refactoring

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
-var canv, cont
-var c = console;
+let canv, cont
+const noise_size = 256
 
-function init(){
-  canv = document.getElementById('output');
-  cont = canv.getContext('2d');
-	myOnnxSession = new onnx.InferenceSession({backendHint: 'webgl'});
-	myOnnxSession.loadModel("./generator.onnx");
-	model = myOnnxSession;
+const rnorm = () => Math.sqrt(-2 * Math.log(1 - Math.random())) * Math.cos(2 * Math.PI * Math.random())
+
+const init = () => {
+  canv = document.getElementById('output')
+  cont = canv.getContext('2d')
+  myOnnxSession = new onnx.InferenceSession({backendHint: 'webgl'})
+  myOnnxSession.loadModel('./generator.onnx')
+  model = myOnnxSession
 }
 
-function rnorm(){
-    return Math.sqrt(-2 * Math.log(1 - Math.random())) * Math.cos(2 * Math.PI * Math.random());
-}
+const generate = () => {
+  const noise= new Float32Array(noise_size)
+  for (let i = 0; i < noise_size; i++) noise[i] = rnorm()
+  noise = new onnx.Tensor(noise, 'float32', [1,256])
 
-function generate(){
-	noise_size = 256;
-  		var noise= new Float32Array(noise_size)
-  		for(i=0; i<noise_size; i=i+1){
-    	noise[i]=rnorm()
-  	}
-  	noise = new onnx.Tensor(noise, 'float32', [1,256]);
-	model.run([noise]).then((output) => {
-          // consume the output
-          const outputTensor = output.values().next().value;
-          drawGeneratedImage(generatedImage = outputTensor.data)
-          console.log(outputTensor)
-        });
+  model.run([noise]).then((output) => {
+    const outputTensor = output.values().next().value;
+    drawGeneratedImage(generatedImage = outputTensor.data)
+    console.log(outputTensor)
+  });
 }
