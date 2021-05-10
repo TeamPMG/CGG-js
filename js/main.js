1f3f50aadf93f2935c230d4bae5bda4b84751876
Haruto Hirakawa <31243896+jpnykw@users.noreply.github.com>
2021-05-11 07:44:13 +0900

    :recycle: Fixed scope pollution

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
+const noiseSize = 256
+
 (() => {
   onload = () => {    
     const rnorm = () => Math.sqrt(-2 * Math.log(1 - Math.random())) * Math.cos(2 * Math.PI * Math.random())
 
-    const generate = (model = null) => {
-      if (model === null) return false
+    const generate = (model = null, context = null) => {
+      if (model === null || context === null) return false
       console.log('called generate function')
 
-      const noise = new Float32Array(noise_size)
-      for (let i = 0; i < noise_size; i++) noise[i] = rnorm()
+      const noise = new Float32Array(noiseSize)
+      for (let i = 0; i < noiseSize; i++) noise[i] = rnorm()
       const tensorNoise = new onnx.Tensor(noise, 'float32', [1, 256])
 
       model.run([tensorNoise]).then((output) => {
         console.log('finish running the model')
         const outputTensor = output.values().next().value
-        drawGeneratedImage(generatedImage = outputTensor.data)
+        context.drawGeneratedImage(generatedImage = outputTensor.data)
         console.log('result', outputTensor)
       });
     }
     
     // Initialize
-    const noise_size = 256
-    const canv = document.querySelector('canvas')
-    const cont = canv.getContext('2d')
-
+    const canvas = document.querySelector('canvas')
+    const context = canvas.getContext('2d')
     const myOnnxSession = new onnx.InferenceSession({backendHint: 'webgl'})
+
+    // Load model
     myOnnxSession.loadModel('generator.onnx').then(() => {
       const model = myOnnxSession
       const button = document.querySelector('button')
-      button.addEventListener('click', () => generate(model))
+      button.addEventListener('click', () => generate(model, context))
       button.removeAttribute('disabled')
     })
   }
