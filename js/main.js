94dff0efd687b683445c4b42e3ad2c34429eb869
mirukuma <mirukuma_tech@icloud.com>
2021-11-27 01:46:20 +0900

    とりあえず動くように

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ const noiseSize = 128;
 const truncationThreshold  = 100;
 
 (() => {
-  onload = () => {    
+  onload = async () => {
     const rnorm = () => Math.sqrt(-2 * Math.log(1 - Math.random())) * Math.cos(2 * Math.PI * Math.random())
 
     const generate = (model = null, context = null) => {
       if (model === null || context === null) return false
-      console.log('called generate function')
-
-      const noise = new Float32Array(noiseSize)
+      console.log('called generate function');
+      const noise = new Float32Array(noiseSize);
       for (let i = 0; i < noiseSize; i++) {
         noise[i] = rnorm();
         if (truncationThreshold != 0) {
@@ -19,29 +18,23 @@ const truncationThreshold  = 100;
           }
         }
       }
-      console.log(noise)
-      const tensorNoise = new onnx.Tensor(noise, 'float32', [1, noiseSize])
-
-      model.run([tensorNoise]).then((output) => {
-        console.log('finish running the model')
-        const outputTensor = output.values().next().value
-        context.drawGeneratedImage(generatedImage = outputTensor.data)
-        console.log('result', outputTensor)
+      console.log(noise);
+      const tensorNoise = new ort.Tensor('float32', noise, [1, noiseSize]);
+      model.run({'x.1':tensorNoise}).then((output) => {
+        console.log('finish running the model');
+        const outputTensor = output['1805']
+        context.drawGeneratedImage(generatedImage = outputTensor.data);
+        console.log('result', outputTensor);
       });
     }
     
     // Initialize
-    
-    const canvas = document.querySelector('canvas')
-    const context = canvas.getContext('2d')
-    const myOnnxSession = new onnx.InferenceSession({backendHint: 'webgl'})
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
 
-    // Load model
-    myOnnxSession.loadModel('generator.onnx').then(() => {
-      const model = myOnnxSession
-      const button = document.querySelector('button')
-      button.addEventListener('click', () => generate(model, context))
-      button.removeAttribute('disabled')
-    })
+    const model = await ort.InferenceSession.create('./generator.onnx');
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => generate(model, context));
+    button.removeAttribute('disabled');
   }
 })()
